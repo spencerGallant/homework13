558fa96c1a0071619fd5da4f9a0483648e560c5c
spencerGallant <spencer.gallant@gmail.com>
2020-04-23 19:22:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ var port = process.env.PORT || 3000;
     res.write("Name: " + name + " <br> Ticker: " + ticker);
     
     // connect to MongoDB
-     const MongoClient = require('mongodb').MongoClient;
+//      const MongoClient = require('mongodb').MongoClient;
 
-     const uri = "mongodb+srv://spencergallant:inkheart3115@cluster0-yzdxb.mongodb.net/test?retryWrites=true&w=majority";
+//      const uri = "mongodb+srv://spencergallant:inkheart3115@cluster0-yzdxb.mongodb.net/test?retryWrites=true&w=majority";
 
-     const client = new MongoClient(uri, { useUnifiedTopology: true });
+//      const client = new MongoClient(uri, { useUnifiedTopology: true });
     
-     connect(client, name, ticker, res);
+//      connect(client, name, ticker, res);
   //res.end();
 //   var test = qobj.x;
    res.end();
