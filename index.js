251170449884ad5d6801f6f276e9dc271cc302d0
spencerGallant <spencer.gallant@gmail.com>
2020-04-23 19:24:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 3000;
 
  http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    var qobj = url.parse(req.url, true).query;
+    var obj = url.parse(req.url, true).query;
     var name = obj.company_name;
     var ticker = obj.ticker;
 //     res.write("Name: " + name + " <br> Ticker: " + ticker);
