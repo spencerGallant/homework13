dd22b5aba0f1b458c20b34ec14d3a6b51f3d071f
spencerGallant <spencer.gallant@gmail.com>
2020-04-23 18:35:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,68 +2,67 @@ var http = require('http');
 var url = require('url');
 var port = process.env.PORT || 3000;
 
-// https://www.oreilly.com/library/view/mastering-nodejs-/9781785888960/e00d3fc1-c435-488c-9aab-d81f377ffe69.xhtml
-// https://gist.github.com/kentbrew/763822
-// https://stackoverflow.com/questions/35408729/express-js-
 
-http.createServer(function (req, res) {
+ http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-pe': 'text/html'});
+     res.write("<h1> HELLO <h1>");
+     res.end();
+        
+//     if (req.url === '/favicon.ico') {
+//         console.log("MAKES IT HERE");	
+//         res.writeHead(200, {'Content-pe': 'image/x-icon'});
+//         // return;
+//     }  
 
-    if (req.url === '/favicon.ico') {
-        console.log("MAKES IT HERE");	
-        res.writeHead(200, {'Content-pe': 'image/x-icon'});
-        // return;
-    }  
-
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var obj = url.parse(req.url, true).query;
-    var name = obj.company_name;
-    var ticker = obj.ticker;
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     var obj = url.parse(req.url, true).query;
+//     var name = obj.company_name;
+//     var ticker = obj.ticker;
     
-    // res.write("Name: " + name + " <br> Ticker: " + ticker);
+//     // res.write("Name: " + name + " <br> Ticker: " + ticker);
     
-    // connect to MongoDB
-    const MongoClient = require('mongodb').MongoClient;
+//     // connect to MongoDB
+//     const MongoClient = require('mongodb').MongoClient;
 
-    const uri = "mongodb+srv://spencergallant:inkheart3115@cluster0-yzdxb.mongodb.net/test?retryWrites=true&w=majority";
+//     const uri = "mongodb+srv://spencergallant:inkheart3115@cluster0-yzdxb.mongodb.net/test?retryWrites=true&w=majority";
 
-    const client = new MongoClient(uri, { useUnifiedTopology: true });
+//     const client = new MongoClient(uri, { useUnifiedTopology: true });
     
-    connect(client, name, ticker, res);
-    //res.end();
+//     connect(client, name, ticker, res);
+//     //res.end();
+ }).listen(port);
 
-}).listen(port);
+// async function connect(client, name, ticker, res) {
+//     client.connect(err => {
+//         const collection = client.db("homework13").collection("companies");
+//         console.log("success connecting!");
 
-async function connect(client, name, ticker, res) {
-    client.connect(err => {
-        const collection = client.db("homework13").collection("companies");
-        console.log("success connecting!");
+//         collection.find().toArray(function (err, items) {
+//             if (err) {
+//                 console.log("Error: " + err);
+//                 return;
+//             }
+//             else {
+//                 found = false;
 
-        collection.find().toArray(function (err, items) {
-            if (err) {
-                console.log("Error: " + err);
-                return;
-            }
-            else {
-                found = false;
+//                 for (i = 0; i < items.length; i++) {
+//                     console.log("in for loop");
+//                     if (items[i].Company == name) {
+//                         res.write(name + "'s ticker is: " + items[i].Ticker);
+//                         found = true;
+//                         break;
+//                     }
+//                     if (items[i].Ticker == ticker) {
+//                         res.write(ticker + " represents " + items[i].Company);
+//                         found = true;
+//                         break;
+//                     }
+//                 }
 
-                for (i = 0; i < items.length; i++) {
-                    console.log("in for loop");
-                    if (items[i].Company == name) {
-                        res.write(name + "'s ticker is: " + items[i].Ticker);
-                        found = true;
-                        break;
-                    }
-                    if (items[i].Ticker == ticker) {
-                        res.write(ticker + " represents " + items[i].Company);
-                        found = true;
-                        break;
-                    }
-                }
-
-                if (!found) {
-                    res.write("The company or ticker you are looking for is not in our database. Sorry!");
-                }
-            }
-        });  //end find
-    });
-}
+//                 if (!found) {
+//                     res.write("The company or ticker you are looking for is not in our database. Sorry!");
+//                 }
+//             }
+//         });  //end find
+//     });
+// }
