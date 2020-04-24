5c885d7c0c97d16285162aa32992dd89d1cb14d6
spencerGallant <spencer.gallant@gmail.com>
2020-04-23 21:28:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var port = process.env.PORT || 3000;
 
      const uri = "mongodb+srv://spencergallant:<inkheart3115>@cluster0-avmpy.mongodb.net/test?retryWrites=true&w=majority";
      console.log("entering the function");
-     MongoClient.connect(uri, function(err,db){
+     MongoClient.connect(uri, name, ticker, res, function(err,db){
       res.write("entering");
 //       .get('/', function(req, res) {
       var dbo = db.db("homework13");
